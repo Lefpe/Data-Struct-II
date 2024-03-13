f20b6a69c5ff988a492228693a4b980f5de2e737
Felipe Macedo <147464434+Lefpe@users.noreply.github.com>
2024-03-13 16:49:38 -0300

    Atualizar o ordem2.c

--- ordem2.c
+++ ordem2.c
@@ -1,108 +1,69 @@
-//a) armazenar dados//
-//b) utilizar lista encadeada simples//
-//c) uma opção emissão de ingressos pelo mesmo cpf, usando insertsort//
-//d) inserir novo ingresso e verificar usuário//
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-//struct dedidaca a organizar a data;
-typedef struct data
-{
+// Struct dedicada a organizar a data
+typedef struct Data {
     int dia;
     int mes;
     int ano;
+} Data;
+
+// Struct de ordenar dados
+typedef struct Ingresso {
+    char nome[50];
+    int cpf;
+    Data dataevento;
+    struct Ingresso *next;
+} Ingresso;
+
+// Lista de ordenação a partir do primeiro ingresso
+struct ListaIngressos {
+    Ingresso *first;
 };
-//struct de ordenar dados//
-typedef struct dados
-{
-    char nome [50];
-    int cpf [11];
-    data *dataevento;
-    struct ingresso *next;
-    
-};
-//listar ordernação a partir do primeiro ingresso//
-struct listaringressos
-{
-    ingresso *first;
-};
 
+// Cadastrar ingressos
+Ingresso *cadastroIngresso() {
+    Ingresso *novo = malloc(sizeof(Ingresso));
 
-//cadastrar ingressos//
-cadastro *cadastroingresso (){
-ingresso  *novo = malloc(sizeof(ingresso));
-//estrutura//
-    printf ("digite seu nome: \n");
-    scanf("%c",novo-> nome);
-    printf ("CPF: \n");
-    scanf ("%d", &novo-> cpf);
+    printf("Digite seu nome: ");
+    scanf("%s", novo->nome);
 
-    printf ("data do evento  Dia, Mes, ano: (use apenas 2 digitos para cada, para ano, os 2 digitos finais) \n");
-    scanf ("%d %d %d", &novo-> datavento.dia, &novo-> dataevento.mes, %novo-> dataevento.ano);
+    printf("CPF: ");
+    scanf("%d", &novo->cpf);
 
-    novo -> next = NULL;
-    
+    printf("Data do evento (Dia Mes Ano): ");
+    scanf("%d %d %d", &novo->dataevento.dia, &novo->dataevento.mes, &novo->dataevento.ano);
 
-    //verificar se há conflitos antes de cadastrar// 
-     Ingresso *atual = listaringressos -> first;
-   
+    novo->next = NULL;
 
-    Ingresso *atual = lista->first;
-    int duplicado = 0;
-
-    while (atual != NULL) {
-        if (atual->cpf == novoIngresso->cpf && memcmp(&atual->dataevento, &novoIngresso->dataevento, sizeof(Data)) == 0) {
-            printf("Duplicidade detectada. Não cadastrado.\n");
-            duplicado = 1;
-            free(novoIngresso);
-            break;
-        }
-        atual = atual->next;
-    }
-  // Cadastrar se não for duplicado
-    if (!duplicado) {
-        // Inserir ordenadamente utilizando InsertionSort//
-        Ingresso *atual = lista->first;
-        Ingresso *anterior = NULL;
-        //verifica data do evento//
-        while (atual != NULL && atual->dataevento.ano < novoIngresso->dataevento.ano) {
-            anterior = atual;
-            atual = atual->next;
-        }
-         while (atual != NULL && atual->dataevento.ano == novoIngresso->dataevento.ano &&
-               atual->dataevento.mes < novoIngresso->dataevento.mes) {
-            anterior = atual;
-            atual = atual->next;
-        }
-
-        while (atual != NULL && atual->dataevento.ano == novoIngresso->dataevento.ano &&
-               atual->dataevento.mes == novoIngresso->dataevento.mes &&
-               atual->dataevento.dia < novoIngresso->dataevento.dia) {
-            anterior = atual;
-            atual = atual->next;
-        }
-
-        if (anterior == NULL) {
-            novoIngresso->next = lista->first;
-            lista->first = novoIngresso;
-        } else {
-            anterior->next = novoIngresso;
-            novoIngresso->next = atual;
-        }
-
-        printf("Ingresso cadastrado!\n");
-    }
- 
+    return novo;
 }
-//comparação de de dados//
-int comparardados(ingresso *ingresso1, ingresso *ingresso2){
 
-
+// Comparação de dados
+int compararDados(Ingresso *ingresso1, Ingresso *ingresso2) {
+    // Implemente a comparação de dados conforme necessário
+    if (ingresso1->dataevento.ano > ingresso2->dataevento.ano)
+        return 1;
+    else if (ingresso1->dataevento.ano < ingresso2->dataevento.ano)
+        return -1;
+    else {
+        if (ingresso1->dataevento.mes > ingresso2->dataevento.mes)
+            return 1;
+        else if (ingresso1->dataevento.mes < ingresso2->dataevento.mes)
+            return -1;
+        else {
+            if (ingresso1->dataevento.dia > ingresso2->dataevento.dia)
+                return 1;
+            else if (ingresso1->dataevento.dia < ingresso2->dataevento.dia)
+                return -1;
+            else
+                return 0;
+        }
+    }
 }
-//inicializar busca por cpf//
+
+// Inicializar busca por CPF
 void buscarPorCPF(struct ListaIngressos lista, int cpf) {
     Ingresso *atual = lista.first;
     printf("Ingressos encontrados para CPF %d:\n", cpf);
@@ -117,8 +78,8 @@ void buscarPorCPF(struct ListaIngressos lista, int cpf) {
     }
 }
 
-// Inserir ordenadamente na lista por data usando o método Insertion Sort//
-void inserirOrdenado(ListaIngressos *lista, Ingresso *novoIngresso) {
+// Inserir ordenadamente na lista por data usando o método Insertion Sort
+void inserirOrdenado(struct ListaIngressos *lista, Ingresso *novoIngresso) {
     Ingresso *atual = lista->first;
     Ingresso *anterior = NULL;
 
@@ -135,36 +96,70 @@ void inserirOrdenado(ListaIngressos *lista, Ingresso *novoIngresso) {
         novoIngresso->next = atual;
     }
 }
- int main (){
-    int options;
 
-    printf ("selecione as opções: 1) cadastrar ingresso \n 2) exibir lista \n 3) buscar ingressos por cpf \n 4) exibir lista ordenada (quicksort) \n sair ");
-    scanf ("%d", &options);
+int main() {
+    struct ListaIngressos lista;
+    lista.first = NULL;
 
-    
+    int opcao;
 
-    do while (options) {
-        case 1:
+    do {
+        printf("\nSelecione as opções:\n");
+        printf("1) Cadastrar ingresso\n");
+        printf("2) Exibir lista\n");
+        printf("3) Buscar ingressos por CPF\n");
+        printf("4) Exibir lista ordenada (quicksort)\n");
+        printf("0) Sair\n");
+        scanf("%d", &opcao);
 
-        break;
+        switch (opcao) {
+            case 1: {
+                Ingresso *novoIngresso = cadastroIngresso();
+                inserirOrdenado(&lista, novoIngresso);
+                break;
+            }
 
-        case 2:
-         
-         break;
+            case 2: {
+                // Exibir lista
+                Ingresso *atual = lista.first;
+                printf("\nLista de Ingressos:\n");
 
-         case 3:
-          
-          break;
+                while (atual != NULL) {
+                    printf("Nome: %s, CPF: %d, Data do Evento: %02d/%02d/%04d\n",
+                           atual->nome, atual->cpf,
+                           atual->dataevento.dia, atual->dataevento.mes, atual->dataevento.ano);
+                    atual = atual->next;
+                }
+                break;
+            }
 
-          case 4:
+            case 3: {
+                // Buscar por CPF
+                int cpfBusca;
+                printf("Digite o CPF para busca: ");
+                scanf("%d", &cpfBusca);
+                buscarPorCPF(lista, cpfBusca);
+                break;
+            }
 
-          break;
+            case 4: {
+                // Exibir lista ordenada (quicksort)
+                // Implemente o método quickSort e a função para exibir a lista ordenada
+                break;
+            }
 
-          
+            case 0: {
+                printf("Saindo do programa.\n");
+                break;
+            }
 
-           default:
-           break;
-    }
+            default: {
+                printf("Opção inválida. Tente novamente.\n");
+                break;
+            }
+        }
+
+    } while (opcao != 0);
+
     return 0;
-
- }
+}
