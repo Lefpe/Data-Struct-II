1f24946a5464f5a1f07e8b3385e9f9c515fdeb2d
Felipe Macedo <147464434+Lefpe@users.noreply.github.com>
2024-03-13 14:30:29 -0300

    Update atividadepontuada.c

--- atividadepontuada.c
+++ atividadepontuada.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+//struct dedidaca a organizar a data;
+typedef struct data
+{
+    int dia;
+    int mes;
+    int ano;
+};
+//struct de ordenar dados//
+typedef struct dados
+{
+    char nome [50];
+    int cpf [11];
+    data *dataevento;
+    struct ingresso *prox;
+    
+};
+//listar ordernação a partir do primeiro ingresso//
+struct listaringressos
+{
+    ingresso *first;
+};
+
+
+//cadastrar ingressos//
+void cadastro (){
+    printf ("digite seu nome: \n");
+    scanf("%c",novo-> nome);
+    printf ("CPF: \n");
+    scanf ("%d", novo ->&cpf);
+
+
+
+
+}
+//comparação de dedados//
+void comparardados(){
+
+}
+//inicializar busca por cpf//
+void buscaporcpf(){
+
+}
